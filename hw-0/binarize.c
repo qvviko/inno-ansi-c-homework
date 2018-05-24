0d56a670051ce0640b2dc000bb6e635cada9a192
Vlad Savchuk <qvviko@gmail.com>
2018-05-24 10:15:09 +0300

    + hw-0 done

--- hw-0/binarize.c
+++ hw-0/binarize.c
@@ -4,38 +4,97 @@
 
 #include "binarize.h"
 
+int *to_binary(unsigned long long number);
+
+void print_bytes(int bins[]);
 
 void binarize_u(unsigned long long x) {
-    int outNum[64];
+    int *result = to_binary(x);
 
-    int i = 0;
-    while (x > 0) {
-        outNum[i] = x % 2;
-        i++;
-        x = x / 2;
-    }
+    print_bytes(result);
 }
 
 void binarize_s(signed long long y) {
-    int outNum[64];
-
     int i = 0;
-    if (y < 0){
-        int sign = 1;
-        y = y*(-1);
+    int sign;
+    if (y < 0) {
+        sign = 1;
+        y = y * (-1);
+    } else {
+        sign = 0;
+    }
+
+    int *result = to_binary(y);
+    if (sign == 1){
+        int j = 63;
+        while (result[j] == 0) {
+            j--;
+        }
+        while (j % 8 != 7) {
+            j++;
+        }
+
+        int c = j;
+        while (c>=0){
+            if (result[c] == 0){
+                result[c] = 1;
+            } else {
+                result[c] = 0;
+            }
+            c--;
+        }
+
+        c =0;
+        while(c!=j){
+            if (result[c] == 1){
+                result[c] = 0;
+                c++;
+            }
+            else{
+                result[c] = 1;
+                break;
+            }
+        }
+        print_bytes(result);
     }
     else{
-        int sign = 0;
+        print_bytes(result);
     }
-    while (y > 0) {
-        outNum[i] = y % 2;
+}
+
+int *to_binary(unsigned long long number) {
+    static int outNum[64];
+    int i = 0;
+
+    while (number > 0) {
+        outNum[i] = number % 2;
         i++;
-        y = y / 2;
+        number = number / 2;
     }
-    i = 63;
-    while (i != -1) {
-        printf("%d", outNum[i]);
-        i--;
+    return outNum;
+}
+
+void print_bytes(int bins[]) {
+    int j = 63;
+    while (bins[j] == 0) {
+        j--;
+    }
+    while (j % 8 != 7) {
+        j++;
     }
 
+    while (j > 7) {
+        int c;
+        for (c = 0; c < 8; c++) {
+            printf("%d", bins[j]);
+            j--;
+        }
+        printf(" ");
+    }
+    int c;
+    for (c = 0; c < 8; c++) {
+        printf("%d", bins[j]);
+        j--;
+    }
+    printf("\n");
 }
